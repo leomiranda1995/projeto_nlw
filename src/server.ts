879a8e827b4831c00c23345cc7e4a70aedac9201
leomiranda1995 <leomiranda1995@gmail.com>
2021-04-19 19:43:14 -0300

    NLW 5 - Aula 1

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (request, response) => {
+  // return response.send("Olá NLW 05"); 
+  return response.json({
+    message: "Olá NLW 05",
+  });
+});
+
+app.post("/", (resquest, response)=>{
+  return response.json({
+    message: "Usuário salvo com sucesso",
+  });
+});
+
+app.listen(3333, ()=> {console.log("Server is running on port 3333")});
